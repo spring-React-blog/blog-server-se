48b4c81f82e20e11a12640e90830d5b7bac48795
seung <ssseung90@gmail.com>
2022-06-27 20:37:41 +0900

    Issue#12 Config 어노테이션 분리

--- src/main/java/com/my/blog/global/config/RedisConfig.java
+++ src/main/java/com/my/blog/global/config/RedisConfig.java
@@ -13,7 +13,6 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableCaching
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
