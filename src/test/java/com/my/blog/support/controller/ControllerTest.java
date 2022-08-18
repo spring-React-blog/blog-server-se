f901049dfd7215c4a1a890fc00cdb93f3dc8df13
seungeun90 <ssseung90@gmail.com>
2022-08-19 00:53:22 +0900

    test properties 추가

--- src/test/java/com/my/blog/support/controller/ControllerTest.java
+++ src/test/java/com/my/blog/support/controller/ControllerTest.java
@@ -34,8 +34,8 @@ public class ControllerTest {
     @DynamicPropertySource
     static void registerPgProperties(DynamicPropertyRegistry registry) {
         registry.add("jwt.secret", () -> "seungeung112220202094885213nfnfkdfm23023jdflsfseungeung112220202094885213nfnfkdfm23023jdflsf");
-        registry.add("jwt.accessTokenValidityInSeconds", () -> 3000);
-        registry.add("jwt.refreshTokenValidityInSeconds", () -> 3000);
+        registry.add("jwt.accessTokenValidityInSeconds", () -> Long.valueOf(3000));
+        registry.add("jwt.refreshTokenValidityInSeconds", () -> Long.valueOf(3000));
 
         registry.add("upload.service", () -> "local");
     }
